88df558f4a7ecb87518408612fa91830466777d3
agnesiss <wlz12003267@icloud.com>
2024-10-25 19:01:46 +0800

    Modified App.js and other files

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I have edited <code>src/App.js</code> and saved the changes.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn How to Google
         </a>
       </header>
     </div>
